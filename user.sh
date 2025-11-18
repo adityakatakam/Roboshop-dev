239a703cb50201dd5ea330ca3247a0ad47e19b2a
Aditya Katakam <aditya.katakam@gmail.com>
2025-11-18 20:06:39 +0530

    modified catalogue.sh

--- user.sh
+++ user.sh
@@ -3,6 +3,8 @@ dnf module enable nodejs:20 -y
 
 dnf install nodejs -y
 
+cp user.service /etc/systemd/system/user.service
+
 useradd roboshop
 
 mkdir /app
@@ -13,4 +15,9 @@ unzip /tmp/user.zip
 
 cd /app
 npm install
+
+systemctl daemon-reload
+
+systemctl enable user
+systemctl start user
 
