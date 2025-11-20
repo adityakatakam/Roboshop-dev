043fc94e9ad987e571b7e0f3b2686d3ca70c81d1
Aditya Katakam <aditya.katakam@gmail.com>
2025-11-20 13:01:23 +0530

    modified shipping.sh

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ dnf install redis -y
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 
-sed -i '/protected-mode-yes/c\protected-mode-no' /etc/redis/redis.conf
+sed -i '/protected-mode yes/c\protected-mode no' /etc/redis/redis.conf
 
 systemctl enable redis
 systemctl start redis
